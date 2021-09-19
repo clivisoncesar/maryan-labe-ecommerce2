a08157aa21087103665405193d894200f86d5f61
clivison <clivison.juris@gmail.com>
2021-09-19 13:28:22 -0300

    Ajustes Iniciais
    Ajustes Iniciais

--- src/App.js
+++ src/App.js
@@ -1,26 +1,154 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Filters } from './components/Filters/Filters';
+import { Products } from './components/Products/Products';
+import { ShoppingCart } from './components/ShoppingCart/ShoppingCart';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 3fr 1fr;
+  padding: 16px;
+  gap: 8px;
+`;
+
+const products = [
+  {
+    id: 1,
+    name: 'Nave Espacial Blaster Version',
+    price: 1000,
+    photo: 'https://i.ibb.co/18ZyWFX/img1.png'
+  },
+  {
+    id: 2,
+    name: 'Foguete Top das Galáxias',
+    price: 18000,
+    photo: 'https://i.ibb.co/ryxNYvD/topdasgalaxias.png'
+  },
+  {
+    id: 3,
+    name: 'Ônibus Espacial #AdeusTerra',
+    price: 3000,
+    photo: 'https://i.ibb.co/5LXV1Kn/adeus-terra.png'
+  },
+  {
+    id: 4,
+    name: 'Foguete na Planta -Previsão de Entrega: 2025',
+    price: 7000,
+    photo: 'https://i.ibb.co/zs2m3Vn/naplanta.png'
+  },
+  {
+    id: 5,
+    name: 'Foguete Nasa Só Para Baixinhos',
+    price: 4000,
+    photo: 'https://i.ibb.co/6wp2fyn/sopara.png'
+  },
+  {
+    id: 6,
+    name: 'Nave Espacial Missão McFly - Back to 1985',
+    price: 10000,
+    photo: 'https://i.ibb.co/DkM9nts/mcfly.png'
+  }
+]
+
+class App extends React.Component {
+  state = {
+    minFilter: 100,
+    maxFilter: 1000,
+    nameFilter: 'Produto',
+    productsInCart: [
+      {
+        id: 6,
+        name: 'Nave Espacial Missão McFly - Back to 1985',
+        price: 10000,
+        photo: 'https://i.ibb.co/DkM9nts/mcfly.png',
+        quantity: 1
+      },
+      {
+        id: 3,
+        name: 'Ônibus Espacial #AdeusTerra',
+        price: 3000,
+        photo: 'https://i.ibb.co/5LXV1Kn/adeus-terra.png',
+        quantity: 2
+      }
+    ]
+  }
+
+  onChangeMinFilter = (event) => {
+    this.setState({minFilter: event.target.value})
+  }
+
+  onChangeMaxFilter = (event) => {
+    this.setState({maxFilter: event.target.value})
+  }
+
+  onChangeNameFilter = (event) => {
+    this.setState({nameFilter: event.target.value})
+  }
+
+  onAddProductToCart = (productId) => {
+    const productInCart = this.state.productsInCart.find(product => productId === product.id)
+
+    if(productInCart) {
+      const newProductsInCart = this.state.productsInCart.map(product => {
+        if(productId === product.id) {
+          return {
+            ...product,
+            quantity: product.quantity + 1
+          }
+        }
+
+        return product
+      })
+
+      this.setState({productsInCart: newProductsInCart})
+    } else {
+      const productToAdd = products.find(product => productId === product.id)
+
+      const newProductsInCart = [...this.state.productsInCart, {...productToAdd, quantity: 1}]
+
+      this.setState({productsInCart: newProductsInCart})
+    }
+  }
+
+  onRemoveProductFromCart = (productId) => {
+    const newProductsInCart = this.state.productsInCart.map((product) => {
+      if(product.id === productId) {
+        return {
+          ...product,
+          quantity: product.quantity - 1
+        }
+      }
+      return product
+    }).filter((product) => product.quantity > 0)
+
+    this.setState({productsInCart: newProductsInCart})
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <Filters
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onChangeMinFilter={this.onChangeMinFilter}            
+          onChangeMaxFilter={this.onChangeMaxFilter}            
+          onChangeNameFilter={this.onChangeNameFilter}                  
+        />
+        <Products 
+          products={products}
+          minFilter={this.state.minFilter}
+          maxFilter={this.state.maxFilter}
+          nameFilter={this.state.nameFilter}
+          onAddProductToCart={this.onAddProductToCart}
+        />
+        <ShoppingCart
+          productsInCart={this.state.productsInCart}
+          onRemoveProductFromCart={this.onRemoveProductFromCart}
+        />
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Products/Products.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import { ProductCard } from './ProductCard'
+import styled from 'styled-components';
+
+const ProductsContainer = styled.div`
+
+`;
+
+const ProductsHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 16px;
+`
+
+const ProductsGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 16px;
+  padding: 16px;
+`
+
+export class Products extends React.Component {
+
+  state = {
+    sort: 'DECRESCENTE'
+  }
+
+  getFilteredAndOrderedList = () => {
+    return this.props.products
+      .sort((a, b) => this.state.sort === 'CRESCENTE' ? a.price - b.price : b.price - a.price)
+  }
+
+  onChangeSort = (event) => {
+    this.setState({sort: event.target.value})
+  }
+
+  render() {
+    const filteredAndOrderedList = this.getFilteredAndOrderedList()
+    return <ProductsContainer>
+      <ProductsHeader>
+        <p>Quantidade de produtos: {filteredAndOrderedList.length}</p>
+        <label>
+          Ordenação:
+          <select value={this.state.sort} onChange={this.onChangeSort}>
+            <option value={'CRESCENTE'}>Crescente</option>
+            <option value={'DECRESCENTE'}>Decrescente</option>
+          </select>
+        </label>
+      </ProductsHeader>
+      <ProductsGrid>
+        {filteredAndOrderedList.map((product) => {
+          return <ProductCard
+            product={product}
+            onAddProductToCart={this.props.onAddProductToCart}
+          />
+        })}
+      </ProductsGrid>
+    </ProductsContainer>
+  }
+}
